4bfdd6dab44551d76d398ecd4bd69becffacb655
vsaldanac <v.saldana.c@gmail.com>
2018-06-16 13:11:47 -0400

    Second commit

---
+++ app/src/main/java/cl/vero/aboutme/EmailFragment.java
@@ -0,0 +1,44 @@
+package cl.vero.aboutme;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmailFragment extends Fragment {
+
+
+    public EmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button contactBtn = view.findViewById(R.id.emailBtn);
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Funciona", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
